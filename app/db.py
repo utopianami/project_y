365958f6a248be0cc77c6c4310f3d3f9ee765751
utopianami <utopianami@gmail.com>
2015-04-12 13:01:59 +0900

    dfdsf

--- app/db.py
+++ app/db.py
@@ -3,9 +3,15 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
+import logging
+
+logging.basicConfig()
+logging.getLogger('sqlalchemy.pool').setLevel(logging.DEBUG)
+
  #'mysql://id:password@localhost/database_name',
 #engine = create_engine('mysql://dbtest:dkagh123@localhost/projecty', convert_unicode=True)
-engine = create_engine('mysql://dbuser:dkagh123@localhost/projecty', convert_unicode=True)
+engine = create_engine('mysql://dbuser:dkagh123@localhost/projecty', convert_unicode=True,
+                       pool_recycle=500, pool_size=5, max_overflow=20, echo=False, echo_pool=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
  
 Base = declarative_base()
