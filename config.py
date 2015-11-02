fdffae701ccfe58ebce637130e7187367f415df1
Sean Moon <seanmoon@Seanui-MacBook-Pro.local>
2015-11-03 04:10:55 +0900

    local

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os.path
-
-# id : dbuser
-# passwrod : dkagh123
-# use sandbox
-
+ 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://dbuser:dkagh123@localhost:3306/sandbox?charset=utf8&use_unicode=0"
 
 ADMINS = frozenset(['youremail@yourdomain.com'])
